4cd3f8525da6b6cee2929f9e1fb02e26d5c85dac
Ali Jaan <aliwsservices@gmail.com>
2025-11-18 01:17:57 +0500

    fields added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -55,5 +55,6 @@ urlpatterns = [
     path('api/user-dashboard-summary/', views.dashboard_summary_user, name='dashboard_summary_user'),
     path('api/get-user-recharge-data/', views.get_user_recharge, name='get_user_recharge'),
     path('api/get-user-account-balance/', views.get_user_account_balance, name='get_user_account_balance'),
-    path('api/update-charges-discount/', views.update_charges_discount, name='update_charges_discount')
+    path('api/update-charges-discount/', views.update_charges_discount, name='update_charges_discount'),
+    path('api/get-company-name/', views.get_company_name, name='get_company_name')
 ]
