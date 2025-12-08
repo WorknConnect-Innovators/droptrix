33cfd1e8bc73096894c38d6788f35ab4f5c19076
Ali Jaan <aliwsservices@gmail.com>
2025-12-08 21:25:59 +0500

    check fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "OPTIONS": {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+            'charset': 'utf8mb4'
         },
         "NAME": "u647796282_droptrix_test",
         "USER": "u647796282_droptrix_test",
