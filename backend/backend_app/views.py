0c72c545d4f6fd23ca7d94dadd56661eb2cb66a7
Ali Jaan <aliwsservices@gmail.com>
2026-02-06 18:34:02 +0500

    Issues Fixed

--- backend/backend_app/views.py
+++ backend/backend_app/views.py
@@ -1234,7 +1234,10 @@ def update_topup(request):
                 aditional_payable_amount = abs(topup_data.payable_amount - Decimal(str(data['payable_amount'])))
                 if aditional_payable_amount > balance_data.account_balance_amount:
                     return JsonResponse({'status': 'success', 'message': 'Insufficient balance. Please recharge your account.'})
-                balance_data.account_balance_amount -= aditional_payable_amount
+                if data['payable_amount'] <= topup_data.payable_amount:
+                    balance_data.account_balance_amount += aditional_payable_amount
+                else:
+                    balance_data.account_balance_amount -= aditional_payable_amount
                 balance_data.save()
                 topup_data.balance_history = balance_data.account_balance_amount
                 topup_data.payable_amount = data['payable_amount']
